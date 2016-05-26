ad853bb2ad59f37b2fc0de2cf1db132e9d9e66df
Caleb Quilley <cquilley@tylercapital.co.uk>
2016-05-26 16:49:31 +0100

    Refactor, started actual compiler

--- rust/brainfuck/src/main.rs
+++ rust/brainfuck/src/main.rs
@@ -1,41 +1,63 @@
-use std::io::prelude::*; // contains file IO
-use std::fs::File; // for file IO
-use std::path::Path; // for file IO
+#![feature(box_syntax, box_patterns)]
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 use std::error::Error;
-use std::env; // contains program environment
+use std::env;
 
-fn main() -> ()
+enum Option
+{
+    Interpret(&str, bool),
+    Compile(CompileFlags)
+}
+
+struct CompileFlags
+{
+    srcpath: &str,
+    out: Option<&str>,
+    opt: u32
+}
+
+fn main()
 {
     // load the source file
     let args : Vec<String> = env::args().collect(); // collect the program arguments
 
-    let mut debug = false;
-    if args.len() < 2 // didn't get source file - first argument is always the program path
+    match parse_args(args)
     {
-        println!("Didn't get source file in argument!");
-        return ();
-    }
+        Interpret(path, debug) =>
+        {
+            let mut src = String::new();
+            match File::open(path)
+            {
+                Ok(f) => f.read_to_string(&src),
+                Err(e) => return println!("Error: {}", e)
+            }
 
-    if args.len() >= 3
-    {
-        debug = args[2] == "1";
-    }
+            interpret(src, debug)
+        },
 
-    let path = Path::new(&args[1]);
-    let mut source_file = match File::open(&path)
-    {
-        Err(why) => panic!("Couldn't open {}: {}", args[1], Error::description(&why)),
-        Ok(file) => file,
-    };
-    let mut src = String::new();
-    match source_file.read_to_string(&mut src)
-    {
-        Err(reason) => panic!("Couldn't read {}: {}", args[1], Error::description(&reason)),
-        Ok(_) => {},
+        Compile(c) =>
+        {
+            match compile(c)
+            {
+                Ok(_) => (),
+                Err(errors) =>
+                {
+                    for e in errors
+                    {
+                        println!("{}", e);
+                    }
+                }
+            }
+        }
     }
-       
-    let source : Vec<char> = src.chars().collect(); // get source as a list of characters
+}
+
+fn interpret(src: &str, debug: bool)
+{
     
+    let source : Vec<char> = src.chars().collect(); // get source as a list of characters
 
     // brainfuck setup 
     let mut pointer : usize = 0;
@@ -47,9 +69,10 @@ fn main() -> ()
         if debug { println!("command: {}; index: {}; tape cell: {}", source[src_index], src_index, pointer); }
         run(&source, source[src_index], &mut tape, &mut pointer, &mut src_index); // run the command
     }
+
 }
 
-fn run(src : &Vec<char>, c : char, tape : &mut Vec<i64>, ptr : &mut usize, src_ptr : &mut usize) -> ()
+fn run(src: &Vec<char>, c: char, tape: &mut Vec<i64>, ptr: &mut usize, src_ptr: &mut usize)
 {
     match c
     {
@@ -137,4 +160,114 @@ fn find_backward(src : &Vec<char>, src_ptr : &mut usize, to_find : char, matchin
         }
         *src_ptr -= 1;
     }
+}
+
+enum ErrType
+{
+    Warning,
+    ParseError,
+    FileError
+}
+
+struct Err
+{
+    errtype: ErrType,
+    msg: String,
+    linenum: u64,
+    colnum: u64
+}
+
+enum ParseTree
+{
+    Span(Vec<Instruction>),
+    Cond(Vec<ParseTree>, box ParseTree), // in order to handle [___[___]___]_____
+}
+
+enum Instruction
+{
+    MoveL,
+    MoveR,
+    OpenLoop(usize),
+    CloseLoop(usize),
+    Inc,
+    Dec,
+    Input,
+    Output
+}
+
+fn compile(c: CompileFlags) -> Result<(), Vec<Err>>
+{
+    let src = match File::open(c.srcpath)
+    {
+        Ok(f) =>
+        {
+            let mut s = String::new();
+            if let Err(e) = f.read_to_string(&mut s)
+            {
+                return vec![Err { errtype: ErrType::FileError, msg: e.description.to_owned(), linenum: 0, colnum: 0 }]
+            }
+
+            s
+        },
+        Err(e) => return vec![Err{ errtype: ErrType::FileError, msg: e.description.to_owned(), linenum: 0, colnum: 0 }]
+    };
+
+    let mut parse_tree = parse(&src)?;
+    if c.opt { opt(&mut parse_tree) }
+    eval_jmps(&mut parse_tree);
+    let text = asm(parse_tree);
+}
+
+struct Span
+{
+    code: Vec<Instruction>,
+    depth: usize
+}
+
+// todo: work out how to convert to a parse tree
+// maybe build a stack of spans with depth
+// and compile an actual tree later?
+fn parse(src: &str) -> ParseTree
+{
+
+    let mut lc = 1;
+    let mut cc = 1;
+    let mut n = 0;
+
+    let mut parens_nests = Vec::new();
+
+    let mut current_span = Vec::new();
+    let mut span_stack = Vec::new();
+    
+    for c in src.chars()
+    {
+        match c
+        {
+            '+' => current_span.push(Instruction::Inc),
+            '-' => current_span.push(Instruction::Dec),
+            '<' => current_span.push(Instruction::MoveL),
+            '>' => current_span.push(Instruction::MoveR),
+            '[' =>
+            {
+                paren_nests.push((n, lc, cc));
+
+            },
+            ']' =>,
+            '\n' => { lc += 1; cc = 1; n += 1; continue },
+            _ => ()
+        }
+
+        cc += 1;
+        n += 1;
+    }
+}
+
+fn opt(tree: &mut ParseTree)
+{
+    
+}
+
+fn eval_jmps(tree: &mut ParseTree)
+{
+    
 }
